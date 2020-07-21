0c6680f20b3358fc8f2a36f92aade755e5020312
Gouerec Pierrick <pgouerec@dhcp115.adfinitas.fr>
2020-07-21 09:57:39 +0200

    [+] Adding new mobile navigation animation and compatibility with banner element

--- src/asset/js/navigation.js
+++ src/asset/js/navigation.js
@@ -1,5 +1,10 @@
 let navbar = document.getElementsByClassName('navigation')[0];
 let navbarOffset = navbar.offsetTop;
+let toggler = document.getElementsByClassName('navigation__toggler')[0];
+let banner = document.getElementsByClassName('banner')[0];
+
+toggler.addEventListener('click', function(){
+});
 
 window.onscroll = function() {
   if (window.pageYOffset >= navbarOffset) {
@@ -17,10 +22,12 @@ function toggle_navigation_bar() {
     document.getElementById('nav-button').classList.add('button--little');
     navigation.children[2].style.display = 'none';
     document.body.style.overflow = 'hidden auto';
+    banner.style.display = 'flex';
   } else {
     document.getElementById('nav-button').classList.remove('button--little');
     navigation.classList.add('navigation--collapsed');
     navigation.children[2].style.display = 'block';
     document.body.style.overflow = 'hidden';
+    banner.style.display = 'none';
   }
 }
